9436bc55bd74c15b567c70b78a43a9cfd628ff8d
soma <soma.ksg@gmail.com>
2025-09-20 09:18:24 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Cold Cortex</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="dist/styles.css" rel="stylesheet">
+</head>
+<body>
+  <?php include __DIR__ . '/includes/header.php'; ?>
+  <div class="container mx-auto px-4">
+    <h1 class="text-5xl font-bold text-center mt-10 font-mono">Cold Cortex</h1>
+    <p class="text-center text-slate-600 pt-1 font-bitcount-ink font-bitcount-ink-vars">Dub Techno powered by ❤️</p>
+  </div>
+</body>
+</html>
