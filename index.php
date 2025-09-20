f532c7574e6d54720f419a4e1039545dabf9605a
soma <soma.ksg@gmail.com>
2025-09-20 23:27:40 +0900

    add picture

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="./dist/styles.css" rel="stylesheet">
 </head>
-<body>
+<body class="bg-black text-white">
   <?php include __DIR__ . '/includes/header.php'; ?>
   <div class="container mx-auto px-4">
     <h1 class="text-5xl font-bold text-center mt-10 font-mono">Cold Cortex</h1>
     <p class="text-center text-slate-600 pt-1 font-bitcount-ink font-bitcount-ink-vars">Dub Techno powered by ❤️</p>
+    <img class="w-70 mx-auto pt-8" src="dist/img/cold_cortex.jpg" alt="Logo">
   </div>
 </body>
 </html>
