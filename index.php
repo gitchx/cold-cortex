ffc18511acf6240ac3fe0378681249eaa6b2ad04
soma <soma.ksg@gmail.com>
2025-09-20 14:46:52 +0900

    fix font

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Cold Cortex</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="dist/styles.css" rel="stylesheet">
+  <link href="./dist/styles.css" rel="stylesheet">
 </head>
 <body>
   <?php include __DIR__ . '/includes/header.php'; ?>
