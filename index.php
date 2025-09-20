17b691ec37b1654d180b18c366ac432d331d0fc0
soma <soma.ksg@gmail.com>
2025-09-21 07:18:08 +0900

    add descrption

--- index.php
+++ index.php
@@ -11,7 +11,13 @@
   <div class="container mx-auto px-4">
     <h1 class="text-5xl font-bold text-center mt-10 font-mono">Cold Cortex</h1>
     <p class="text-center text-slate-600 pt-1 font-bitcount-ink font-bitcount-ink-vars">Dub Techno powered by ❤️</p>
-    <img class="w-70 mx-auto pt-8" src="dist/img/cold_cortex.jpg" alt="Logo">
+    <img class="w-70 mx-auto pt-8 rounded-full" src="dist/img/cold_cortex.jpg" alt="Logo">
+    <div class="text-center mt-8 max-w-3xl mx-auto font-mono text-xs">
+      <p>Tsubasa Arakawa と Soma Kosugi のユニット「Cold Cortex」です。</p>
+      <p>Dub Techno を中心に様々なジャンルを横断しながら、<br>音楽制作とライブを行っています。</p>
+      <br>
+      <p>Electron, Ableton Push3とギターを使用したライブセットを展開し、<br>独自のサウンドを追求しています。</p>
+    </div>
   </div>
 </body>
 </html>
